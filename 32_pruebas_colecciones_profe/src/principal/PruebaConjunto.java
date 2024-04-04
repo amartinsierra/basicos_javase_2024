ae7c305c64e6c6fc7a91f89832a528450ece8d45
Profesormanana <Profesormanana@A13E100.mshome.net>
2024-04-04 12:23:40 +0200

    ejercicio 38

---
+++ 32_pruebas_colecciones_profe/src/principal/PruebaConjunto.java
@@ -0,0 +1,20 @@
+package principal;
+
+import java.util.HashSet;
+
+public class PruebaConjunto {
+
+	public static void main(String[] args) {
+		HashSet<Double> nums=new HashSet<Double>();
+		nums.add(30.7);
+		nums.add(80.0);
+		nums.add(21.3);
+		for(Double s:nums) {
+			s=s+1;
+		}
+		for(Double s:nums) {
+			System.out.println(s);
+		}
+	}
+
+}
