20763dce4f6dda6752fe3b17d5c9e6b36d3f689d
Profesormanana <Profesormanana@A13E100.mshome.net>
2024-04-04 10:31:05 +0200

    36 y 37

---
+++ 36_uso_javabeans/src/principal/Prueba.java
@@ -0,0 +1,16 @@
+package principal;
+
+import java.util.ArrayList;
+
+import model.Ciudad;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		Ciudad ciudad=new Ciudad("prueba 1", 300000, 20.4);
+		ArrayList<Ciudad> ciudades=new ArrayList<Ciudad>();
+		ciudades.add(ciudad);
+
+	}
+
+}
